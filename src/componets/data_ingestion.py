cd604839b0c8dff655ffea660ba1f982ff07a0e4
Shubh9165 <shubhpatel915@gmail.com>
2025-11-05 15:14:31 +0530

    data ingestion and transformation

--- src/componets/data_ingestion.py
+++ src/componets/data_ingestion.py
@@ -5,6 +5,9 @@ from src.logger import logging
 from src.exception import CustomExcption   # ✅ fixed spelling
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
+from src.componets.data_transformation import DataTransformation
+from src.componets.data_transformation import DataTransformationConfig
+
 
 
 @dataclass
@@ -22,32 +25,35 @@ class DataIngestion:
         logging.info("The data ingestion process has been initiated")
 
         try:
-            # ✅ fixed invalid escape sequence (used raw string)
             df = pd.read_csv(r'notebook\data\StudentsPerformance.csv')
             logging.info("Dataset has been successfully read")
 
-            # ✅ ensure folder exists
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
-            # ✅ save raw data
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
             logging.info("Raw dataset saved successfully")
 
-            # ✅ split data
             logging.info("Train-test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.20, random_state=42)
 
-            # ✅ save train and test data
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
             logging.info("Data ingestion process completed successfully")
 
+            # ✅ Add this return statement
+            return (
+             self.ingestion_config.train_data_path,
+              self.ingestion_config.test_data_path
+            )
+
         except Exception as e:
-            # ✅ fixed spelling (CustomException)
             raise CustomExcption(e, sys)
 
 
 if __name__ == "__main__":
-    obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import sys
+import os
+from src.exception import CustomExcption
+import pickle
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomExcption(e, sys)
